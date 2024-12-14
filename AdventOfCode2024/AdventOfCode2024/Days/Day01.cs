3dc60bd42fb9d3d29e5b1d56304f58ccab94a79a
Mylène Fleury <my@nexplore.ch>
2024-12-14 22:36:55 +0100

    Add c# console app with empty day 01

---
+++ AdventOfCode2024/AdventOfCode2024/Days/Day01.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode2024.Days
+{
+    public static class Day01
+    {
+        public static void Solution()
+        {
+            Console.WriteLine("Haloooooo");
+        }
+    }
+}
---
+++ AdventOfCode2024/AdventOfCode2024/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using AdventOfCode2024.Days;
+
+Console.WriteLine("Hi, welcome to advent of code 2024");
+
+var day = getDayInput();
+
+switch (day)
+{
+    case 1:
+        Day01.Solution();
+        break;
+    default:
+        Console.WriteLine($"The day {day} does not exist.");
+        break;
+}
+
+int getDayInput()
+{
+    var isDayValidInt = false;
+    int day = 0;
+
+    while (isDayValidInt == false)
+    {
+        Console.Write("Tell me which day you want to execute: ");
+        var dayInput = Console.ReadLine();
+
+        isDayValidInt = int.TryParse(dayInput, out day);
+
+        if (!isDayValidInt)
+        {
+            Console.WriteLine("Your input is not a number!");
+        }
+    }
+    return day;
+}
+
